36d1fb48e021b4b2854b54c1738a0bdabbdaa28e
FelipeD4ntas <felipeicedantas@gmail.com>
2022-06-19 14:12:46 -0300

    Firebase update
    implementado salvar arquivos no firebase

---
+++ bancoDados.js
@@ -0,0 +1,117 @@
+const form = document.querySelector('#form');
+let saldoPositivo = 0;
+let saldoNegativo = 0;
+let saldoAtual = 0;
+let banco = buscarTarefaBD();
+
+function adicionarTransacaoBD() {
+    localStorage.setItem('dados', JSON.stringify(banco));
+};
+
+function buscarTarefaBD() {
+    const bancoString = localStorage.getItem('dados');
+    const bancoObj = JSON.parse(bancoString);
+
+    return bancoObj
+};
+
+function carregarPag() {
+    if (banco === null) {
+        banco = [];
+    } else {
+        banco.forEach(({ nomeTransacao, valorTansacao }) => {
+            addTransacoesContainer(nomeTransacao, valorTansacao);
+        });
+    };
+};
+
+function imprimirSaldoPositivoOuNegativoNaTela(money, saldo) {
+    money.textContent = `R$ ${saldo.toFixed(2)}`;
+};
+
+function apagarTransacao(event) {
+    const elementoClicado = event.target;
+    const deletar = elementoClicado.dataset.deletar
+    const itemTransacao = document.querySelector(`[data-nome="${deletar}"]`);
+    const nomeItemTransacao = itemTransacao.dataset.nome;
+    const valorItemTransacao = itemTransacao.dataset.valor;
+    console.log(nomeItemTransacao, valorItemTransacao)
+
+    if (deletar) {
+        saldoAtual -= valorItemTransacao;
+        imprimirBalancoTotalNaTela(saldoAtual);
+        
+        if (valorItemTransacao < 0) {
+            saldoNegativo -= valorItemTransacao
+            imprimirSaldoPositivoOuNegativoNaTela(moneyMinus, saldoNegativo);
+        }
+
+        if (valorItemTransacao > 0) {
+            saldoPositivo -= valorItemTransacao
+            imprimirSaldoPositivoOuNegativoNaTela(moneyPlus, saldoPositivo);
+        }
+        
+        itemTransacao.remove();
+        banco = banco.filter((item) => item.nomeTransacao !== nomeItemTransacao);
+        adicionarTransacaoBD();
+    };
+};
+
+function imprimirTransacoesNaTela(classe, nomeTransacao, valorTansacao) {
+    transactions.innerHTML +=
+        `<li  class="${classe}" data-nome="${nomeTransacao}" data-valor="${valorTansacao}">
+          ${nomeTransacao.toUpperCase()} <span>R$ ${valorTansacao.toFixed(2)}</span>
+          <button class="delete-btn" data-deletar="${nomeTransacao}">x</button>
+        </li>`;
+};
+
+function imprimirBalancoTotalNaTela(saldoAtual) {
+    balance.textContent = `R$${(saldoAtual).toFixed(2)}`;
+};
+
+function addTransacoesContainer(nomeTransacao, valorTansacao) {
+    const valorPositivo = valorTansacao > 0;
+    const valorNegativo = valorTansacao < 0;
+    const classeMinusPlus = valorNegativo ? 'minus' : 'plus';
+
+    imprimirTransacoesNaTela(classeMinusPlus, nomeTransacao, valorTansacao);
+
+    if (valorPositivo) {
+        saldoPositivo += valorTansacao;
+        imprimirSaldoPositivoOuNegativoNaTela(moneyPlus, saldoPositivo);
+    };
+
+    if (valorNegativo) {
+        saldoNegativo += valorTansacao;
+        imprimirSaldoPositivoOuNegativoNaTela(moneyMinus, saldoNegativo);
+    };
+
+    saldoAtual = saldoPositivo + saldoNegativo;
+    imprimirBalancoTotalNaTela(saldoAtual);
+}
+
+function adicionarTransacao(event) {
+    event.preventDefault();
+
+    if (text.value && amount.value) {
+        const nomeTransacao = text.value;
+        const valorTansacao = Number(amount.value);
+
+        banco.push({
+            nomeTransacao,
+            valorTansacao
+        });
+
+        adicionarTransacaoBD(banco);
+        addTransacoesContainer(nomeTransacao, valorTansacao);
+    } else {
+        alert('É necessário adicionar o nome e também o valor da transação.');
+    };
+
+    form.reset();
+    text.focus();
+};
+
+window.addEventListener('load', carregarPag);
+form.addEventListener('submit', adicionarTransacao);
+transactions.addEventListener('click', apagarTransacao);
--- script.js
+++ script.js
@@ -1,46 +1,50 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js';
+import { getFirestore, collection, doc, addDoc, deleteDoc, onSnapshot, getDocs} from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBy3CeoNVuro57QCQ7WL9Vw2pzOWBajpYc",
+    authDomain: "appcontrolegastos-723c6.firebaseapp.com",
+    projectId: "appcontrolegastos-723c6",
+    storageBucket: "appcontrolegastos-723c6.appspot.com",
+    messagingSenderId: "356737549891",
+    appId: "1:356737549891:web:bc9190c4b8a5448a9ab6f6",
+    measurementId: "G-QRYLZD386C"
+};
+  
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+const db = getFirestore(app);
+const documentGastos = collection(db, 'gastos');
+
 const form = document.querySelector('#form');
 let saldoPositivo = 0;
 let saldoNegativo = 0;
 let saldoAtual = 0;
-let banco = buscarTarefaBD();
-
-function adicionarTransacaoBD() {
-    localStorage.setItem('dados', JSON.stringify(banco));
-};
-
-function buscarTarefaBD() {
-    const bancoString = localStorage.getItem('dados');
-    const bancoObj = JSON.parse(bancoString);
-
-    return bancoObj
-};
-
-function carregarPag() {
-    if (banco === null) {
-        banco = [];
-    } else {
-        banco.forEach(({ nomeTransacao, valorTansacao }) => {
-            addTransacoesContainer(nomeTransacao, valorTansacao);
-        });
-    };
-};
 
 function imprimirSaldoPositivoOuNegativoNaTela(money, saldo) {
     money.textContent = `R$ ${saldo.toFixed(2)}`;
 };
 
+function imprimirBalancoTotalNaTela(saldoAtual) {
+    balance.textContent = `R$ ${(saldoAtual).toFixed(2)}`;
+};
+
+
 function apagarTransacao(event) {
     const elementoClicado = event.target;
     const deletar = elementoClicado.dataset.deletar
-    const itemTransacao = document.querySelector(`[data-nome="${deletar}"]`);
-    const nomeItemTransacao = itemTransacao.dataset.nome;
-    const valorItemTransacao = itemTransacao.dataset.valor;
-    console.log(nomeItemTransacao, valorItemTransacao)
 
     if (deletar) {
+        const itemTransacao = document.querySelector(`[data-nome="${deletar}"]`);
+        const idItemTransacao = itemTransacao.dataset.nome;
+        const valorItemTransacao = itemTransacao.dataset.valor;
+
         saldoAtual -= valorItemTransacao;
         imprimirBalancoTotalNaTela(saldoAtual);
-        
+        deleteDoc(doc(db, 'gastos', idItemTransacao));
+
         if (valorItemTransacao < 0) {
             saldoNegativo -= valorItemTransacao
             imprimirSaldoPositivoOuNegativoNaTela(moneyMinus, saldoNegativo);
@@ -51,22 +55,7 @@ function apagarTransacao(event) {
             imprimirSaldoPositivoOuNegativoNaTela(moneyPlus, saldoPositivo);
         }
         
-        itemTransacao.remove();
-        banco = banco.filter((item) => item.nomeTransacao !== nomeItemTransacao);
-        adicionarTransacaoBD();
     };
-};
-
-function imprimirTransacoesNaTela(classe, nomeTransacao, valorTansacao) {
-    transactions.innerHTML +=
-        `<li  class="${classe}" data-nome="${nomeTransacao}" data-valor="${valorTansacao}">
-          ${nomeTransacao.toUpperCase()} <span>R$ ${valorTansacao.toFixed(2)}</span>
-          <button class="delete-btn" data-deletar="${nomeTransacao}">x</button>
-        </li>`;
-};
-
-function imprimirBalancoTotalNaTela(saldoAtual) {
-    balance.textContent = `R$${(saldoAtual).toFixed(2)}`;
 };
 
 function addTransacoesContainer(nomeTransacao, valorTansacao) {
@@ -74,35 +63,59 @@ function addTransacoesContainer(nomeTransacao, valorTansacao) {
     const valorNegativo = valorTansacao < 0;
     const classeMinusPlus = valorNegativo ? 'minus' : 'plus';
 
-    imprimirTransacoesNaTela(classeMinusPlus, nomeTransacao, valorTansacao);
-
     if (valorPositivo) {
         saldoPositivo += valorTansacao;
+        
         imprimirSaldoPositivoOuNegativoNaTela(moneyPlus, saldoPositivo);
     };
 
     if (valorNegativo) {
         saldoNegativo += valorTansacao;
+        
         imprimirSaldoPositivoOuNegativoNaTela(moneyMinus, saldoNegativo);
     };
 
     saldoAtual = saldoPositivo + saldoNegativo;
     imprimirBalancoTotalNaTela(saldoAtual);
-}
+};
+
+onSnapshot(documentGastos, querySnapshot => {
+    const listaGastos = querySnapshot.docs.reduce((acc, doc) => {
+        const { nomeTransacao, valorTansacao } = doc.data();
+
+        acc += `<li class="" data-nome="${doc.id}" data-valor="${valorTansacao}">
+          ${nomeTransacao.toUpperCase()} <span>R$ ${valorTansacao.toFixed(2)}</span>
+          <button class="delete-btn" data-deletar="${doc.id}">x</button>
+        </li>`;
+
+        return acc
+    }, '');
+
+    transactions.innerHTML = listaGastos
+});
+
+getDocs(collection(db, 'gastos'))
+    .then(querySnapshot => {
+        querySnapshot.docs.forEach(doc => {
+            const { nomeTransacao, valorTansacao } = doc.data();
+
+            addTransacoesContainer(nomeTransacao, valorTansacao);
+        })
+    })
+    .catch(console.log);
 
 function adicionarTransacao(event) {
     event.preventDefault();
-
+    
     if (text.value && amount.value) {
         const nomeTransacao = text.value;
         const valorTansacao = Number(amount.value);
 
-        banco.push({
+        addDoc(documentGastos, {
             nomeTransacao,
             valorTansacao
-        });
+        })
 
-        adicionarTransacaoBD(banco);
         addTransacoesContainer(nomeTransacao, valorTansacao);
     } else {
         alert('É necessário adicionar o nome e também o valor da transação.');
@@ -112,6 +125,5 @@ function adicionarTransacao(event) {
     text.focus();
 };
 
-window.addEventListener('load', carregarPag);
 form.addEventListener('submit', adicionarTransacao);
 transactions.addEventListener('click', apagarTransacao);
